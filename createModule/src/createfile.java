a02441c88963bf92cda2114391031f6ee3041b96
Ram Sundar <tourams@outlook.com>
2024-01-17 14:58:43 +0000

    addition file created

---
+++ createModule/src/createfile.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.math.*;
+import java.lang.*;
+public class createfile {
+
+    public static void main(String[] args)
+    {
+        int a=50,b=50;
+        int c=0;
+        c=a+b;
+        System.out.println(c);
+
+    }
+}
