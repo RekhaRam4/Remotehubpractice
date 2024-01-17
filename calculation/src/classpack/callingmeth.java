2ce69333c800d226db8cce6d77713e7526e8ac38
Ram Sundar <tourams@outlook.com>
2024-01-17 16:02:01 +0000

    class package created

---
+++ calculation/src/classpack/callingmeth.java
@@ -0,0 +1,10 @@
+package classpack;
+
+public class callingmeth {
+    public String name="Naren";
+    public int roll_no;
+    public void disp_name()
+    {
+        System.out.println("Display name: "+name);
+    }
+}
