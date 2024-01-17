4a0316ee3dae4f032eae000a4fdd12ffe708fff8
Ram Sundar <tourams@outlook.com>
2024-01-17 16:02:01 +0000

    collection package created

---
+++ calculation/src/classpack/classdemo.java
@@ -0,0 +1,13 @@
+package classpack;
+import java.io.*;
+import java.lang.*;
+public class classdemo {
+
+    public static void main(String[] args) {
+     callingmeth  demo= new callingmeth();
+     demo.disp_name();
+     demo.roll_no=1233;
+     System.out.println("Roll no : "+demo.roll_no);
+
+    }
+}
